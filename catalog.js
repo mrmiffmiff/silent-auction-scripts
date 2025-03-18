ebb0c55797b887bdcc909c63b68c30d3a9e57ef2
Robert Eisenman <robert.t.eisenman@gmail.com>
2025-03-18 11:51:33 -0700

    the rest of the script (it works)

--- catalog.js
+++ catalog.js
@@ -1,3 +1,32 @@
-function generateCatalog(id) {
-    const sheet = SpreadsheetApp.openById(id).getSheetByName('Events');
+function generateCatalog() {
+    const sheet = SpreadsheetApp.openById('REDACTED').getSheetByName('Events');
+    const data = sheet.getRange('A2:P3').getValues();
+    const doc = DocumentApp.create('Catalog Test');
+    const body = doc.getBody();
+
+    data.forEach(row => {
+        const itemName = row[0];
+        const itemNumber = row[2];
+        const itemDesc = row[4];
+        const itemDets = row[5];
+        const startBid = row[14];
+        const dispName = row[10];
+
+        const header = body.appendParagraph(`${itemNumber}. ${itemName}`);
+        header.editAsText().setBold(true);
+        const description = body.appendParagraph(itemDesc);
+        description.editAsText().setBold(false);
+        body.appendParagraph("");
+        const dets = body.appendParagraph(`Item/Event Details: ${itemDets}`);
+        dets.editAsText().setBold(0, 18, true);
+        const bid = body.appendParagraph(`Starting Bid: ${startBid}`);
+        bid.editAsText().setBold(0, 12, true);
+        const name = body.appendParagraph(`Thanks To: ${dispName}`);
+        name.editAsText().setBold(0, 9, true);
+        body.appendParagraph("");
+        body.appendParagraph("");
+    });
+
+    doc.saveAndClose();
+    return doc;
 }
